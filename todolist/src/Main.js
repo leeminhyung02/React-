f15e437434f50e3da04fd9b62e3041c708f0b60b
leeminhyung02 <mi087033@gmail.com>
2024-11-05 17:50:44 +0900

    TodoList만들기

--- todolist/src/Main.js
+++ todolist/src/Main.js
@@ -1,9 +1,38 @@
+import React, { useState } from 'react';
+import './Main.css';
+
 function Main(){
+
+	let [inputvalue, SetInputvalue] = useState('');
+	function change(e){
+		SetInputvalue(e.target.value);
+	}
+
+	let [TodoList, SetTodoList] = useState([]);
+
+	function addTodoList(){
+		SetTodoList([...TodoList, inputvalue]);
+		SetInputvalue('');
+	}
+
+
 	return(
-		<div>
-			todoList
+		<div className='container'>
+			<h1 className='header'>TODO LIST</h1>
+			<input value={inputvalue}  onChange={change} type='text' className='inputer' placeholder='할일 입력'></input>
+			<button onClick={addTodoList} className='plusbtn'>+</button>
+			<ul >
+				{
+					TodoList.map((value)=>{
+						return (
+							<div className='List'>
+								<input type='checkbox'/><li >{value}</li>
+							</div>
+					)})
+				}
+			</ul>
 		</div>
-	)
-}
+	);
+};
 
 export default Main;
